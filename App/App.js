4e14976d9ac3eab89147423337ae2a29defd8125
yeonjelee <sjyj040597@gmail.com>
2025-05-28 14:31:26 +0900

    앱 기본 구조를 위한 파일들 업로드

---
+++ App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import MapCam from './screens/MapCam';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="MapCam" component={MapCam} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
